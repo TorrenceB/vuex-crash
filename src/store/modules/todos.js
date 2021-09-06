cb23807d3d974d976a2c9644db121bd106cc0fd8
Torrence Brown <torrenceb90@gmail.com>
2021-09-06 17:27:44 -0600

    Added delete todo action

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -12,17 +12,32 @@ export default {
   },
 
   actions: {
-    async fetchTodos() {
+    async fetchTodos({ commit }) {
       const response = await axios.get(
         "https://jsonplaceholder.typicode.com/todos"
       );
 
-      console.log(response.data);
-      //   commit(response.data);
+      commit("setTodos", response.data);
+    },
+    async addTodo({ commit }, title) {
+      const response = await axios.post(
+        "https://jsonplaceholder.typicode.com/todos",
+        { title, completed: false }
+      );
+
+      commit("newTodo", response.data);
+    },
+    async deleteTodo({ commit }, id) {
+      await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+
+      commit("removeTodo", id);
     },
   },
 
   mutations: {
     setTodos: (state, todos) => (state.todos = todos),
+    newTodo: (state, newTodo) => state.todos.unshift(newTodo),
+    removeTodo: (state, id) =>
+      (state.todos = state.todos.filter((todo) => todo.id !== id)),
   },
 };
