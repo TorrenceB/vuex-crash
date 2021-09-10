a6faa7d8b3d510c4376540eaf53322f06782e9de
Torrence Brown <torrenceb90@gmail.com>
2021-09-10 07:16:37 -0600

    Created filtertodos action.

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -32,6 +32,18 @@ export default {
 
       commit("removeTodo", id);
     },
+    async filterTodos({ commit }, e) {
+      //   Get selected number
+      const limit = parseInt(
+        e.target.options[e.target.options.selectedIndex].innerText
+      );
+
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/todos?_limit=${limit}`
+      );
+
+      commit("setTodos", response.data);
+    },
   },
 
   mutations: {
